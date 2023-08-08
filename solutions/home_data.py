65483d7a97ee7c23503538cec91607e5a9e9036b
stevenrhett <johnsonsteven18@gmail.com>
2023-08-08 10:24:25 -0400

    "finished home-data problem"

--- solutions/home_data.py
+++ solutions/home_data.py
@@ -1,23 +1,39 @@
-import csv
+def load_data(file_path):
+    categories = []
+    data = []
+    with open(file_path, mode='r') as csv_file:
+        for i, line in enumerate(csv_file):
+            line = line.strip()
+            if i == 0:
+                categories = line.split(',')
+            else:
+                row = line.split(',')
+                dictionary = {}
+                for j, category in enumerate(categories):
+                    dictionary[category] = row[j]
+                data.append(dictionary)
+    return data
 
-# Create a list to store the house data
-houses = []
 
-# Read data from the CSV file and extract relevant information
-with open('data/home_data.csv', 'r') as file:
-    reader = csv.reader(file)
-    header = next(reader)  # Skip the header row
-    for row in reader:
-        price, zipcode, condition, sqft_living = float(
-            row[2]), row[0], row[3], int(row[5])
-        houses.append((price, zipcode, condition, sqft_living))
+data = load_data('data/home_data.csv')
 
-# Sort the houses based on price in ascending order
-houses.sort(key=lambda x: x[0])
 
-# Display the 10 least expensive houses
-print("10 Least Expensive Houses:")
-for house in houses[:10]:
-    price, zipcode, condition, sqft_living = house
-    print(
-        f"ZIP Code: {zipcode}, Condition: {condition}, Living Square Feet: {sqft_living}")
+def get_10_least_expensive_houses(data):
+    data.sort(key=lambda x: int(x['price']))
+    return data[:10]
+
+
+def print_houses(houses):
+    print("10 Least Expensive Houses:")
+    print("{:<10} {:<10} {:<10} {:<10}".format(
+        'Price', 'ZIP Code', 'Condition', 'Living Sqft'))
+    for house in houses:
+        print("{:<10} {:<10} {:<10} {:<10}".format(
+            house['price'], house['zipcode'], house['condition'], house['sqft_living']))
+
+
+if __name__ == "__main__":
+    file_path = "data/home_data.csv"
+    data = load_data(file_path)
+    least_expensive_houses = get_10_least_expensive_houses(data)
+    print_houses(least_expensive_houses)
